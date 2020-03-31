6c60ba4d16212f24ae7c90cc58b55e57f0c05162
Python-kid <nillyw31@gmail.com>
2020-03-31 18:08:38 +0300

    Deployed to heroku

--- loan/forms.py
+++ loan/forms.py
@@ -4,25 +4,6 @@ from wtforms.validators import DataRequired, Length, ValidationError
 from loan.models import *
 
 
-class MemberForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=5, max=30)])
-    id_number = StringField('Id_Number', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired(), Length(min=3, max=20)])
-    submit = SubmitField('Save')
-
-    def validate_id_number(self, id_number):
-        member = Member.query.filter_by(id_number=id_number.data).first()
-        if member:
-            raise ValidationError('The id number is already registered to the system !')
-
-
-class CalculatorForm(FlaskForm):
-    loan_amount = IntegerField('Loan Amount', validators=[DataRequired()])
-    period = IntegerField('Period', validators=[DataRequired()])
-    interest_rate = IntegerField('Interest Rate ', validators=[DataRequired()])
-    submit = SubmitField('Save')
-
-
 class LoanForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=5, max=30)])
     id_number = StringField('Id_Number', validators=[DataRequired()])
@@ -33,6 +14,6 @@ class LoanForm(FlaskForm):
     submit = SubmitField('Save')
 
     def validate_id_number(self, id_number):
-        member = Member.query.filter_by(id_number=id_number.data).first()
+        member = Loan.query.filter_by(id_number=id_number.data).first()
         if member:
             raise ValidationError('The id number is already registered to the system !')
--- loan/routes.py
+++ loan/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from loan import app, db
-from loan.forms import LoanForm, MemberForm, CalculatorForm
-from loan.models import Loan,  Member, Calculator
+from loan import app
+from loan.forms import *
+from loan.models import *
 
 
 @app.route('/')
@@ -10,44 +10,16 @@ def home():
     return render_template('home.html', loans=loans)
 
 
-# @app.route('/new_member', methods=['GET', 'POST'])
-# def new_member():
-#     form = MemberForm()
-#     if form.validate_on_submit():
-#         member = Member(name=form.name.data, id_number=form.id_number.data, location=form.location.data)
-#         db.session.add(member)
-#         db.session.commit()
-#         flash('Member added successfully !', 'success')
-#         return redirect(url_for('calculator'))
-#     return render_template('new_member.html', form=form)
-
-
-# @app.route('/calculator', methods=['GET', 'POST'])
-# def calculator():
-#     form = CalculatorForm()
-#     if form.validate_on_submit():
-#         try:
-#             total_loan = round(form.loan_amount.data * form.interest_rate.data / 100 * form.period.data)
-#             amount = Calculator(loan_amount=form.loan_amount.data, interest_rate=form.interest_rate.data,
-#                                 period=form.period.data, total_loan=total_loan)
-#             db.session.add(amount)
-#             db.session.commit()
-#             flash(f'Successful')
-#             return redirect(url_for('home'))
-#         except ValueError:
-#             return redirect(url_for('calculator'))
-#     return render_template('calculator.html', form=form)
-
-
 @app.route('/loan', methods=['GET', 'POST'])
 def loan():
     form = LoanForm()
     if form.validate_on_submit():
         try:
-            amount = Calculator(loan_amount=form.loan_amount.data, interest_rate=form.interest_rate.data,
-                                period=form.period.data)
-            member = Member(name=form.name.data, id_number=form.id_number.data, location=form.location.data)
-            db.session.add(amount, member)
+            total_loan = round(form.loan_amount.data * form.interest_rate.data / 100 * form.period.data)
+            amount = Loan(loan_amount=form.loan_amount.data, interest_rate=form.interest_rate.data,
+                          period=form.period.data, total_loan=total_loan, name=form.name.data,
+                          id_number=form.id_number.data, location=form.location.data)
+            db.session.add(amount)
             db.session.commit()
         except ValueError:
             return redirect(url_for('loan'))
